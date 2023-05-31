807444600aac5a0bdec67185d2ce3b10b8e62836
Timur Tuleuov <timmmur123@gmail.com>
2023-05-31 19:32:28 +0600

    Made working API post

--- src/services/post.service.ts
+++ src/services/post.service.ts
@@ -2,20 +2,20 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map, catchError, tap } from 'rxjs/operators';
 import { Observable, of } from 'rxjs';
-import { Post, PostResponse } from 'src/interfaces/post';
+import { Post } from 'src/interfaces/post';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
-  baseUrl = "http://localhost:4200";
+
   url = "http://localhost:8000/api/post/";
 
   constructor(private http: HttpClient) { 
     this.getData();
   }
-  getData(): Observable<PostResponse[]> {
-    return this.http.get<PostResponse[]>(this.url);
+  getData(): Observable<Post[]> {
+    return this.http.get<Post[]>(this.url);
     
   }
   
