518bd5a474eb9849138ffa5229a30949ce12de89
Timur Tuleuov <timmmur123@gmail.com>
2023-06-01 23:17:52 +0600

    Made new component Signup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,13 +13,15 @@ import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
     HeaderComponent,
-    LoginComponent
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
