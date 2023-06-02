8187a107a00bee9be7968592d0a40f792661f25f
Timur Tuleuov <timmmur123@gmail.com>
2023-06-02 22:57:11 +0600

    Installed slug library

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from 'src/services/login.service';
+import * as slug from 'slug'
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit{
+    slug_name!: string;
   
-    constructor(public loginService: LoginService){}
+    constructor(public loginService: LoginService, private router: Router){}
     ngOnInit(): void {
     }
-    
+    slugIt(username: string){
+      this.slug_name = slug(username)
+     this.router.navigate(['/user'], this.slug_name)
+    }
 
 }
