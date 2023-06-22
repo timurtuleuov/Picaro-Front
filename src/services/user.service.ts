c23bc90cd62a3dba5210eed80908c31b40ba72dd
Timur Tuleuov <timmmur123@gmail.com>
2023-06-22 23:15:43 +0600

    Little fix of service

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from 'src/interfaces/user';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
@@ -8,8 +9,8 @@ export class UserService {
 
   constructor(private http: HttpClient) { }
 
-  getUserInfo(slug_name: string){
+  getUserInfo(slug_name: string): Observable<User> {
     const url = `http://localhost:8000/api/user/by_slug/${slug_name}/`
-    this.http.get<User>(url);
+    return this.http.get<User>(url);
   }
 }
