638ac488ca29243e327944eb9ba5f9ea6cda7a53
Timur Tuleuov <timmmur123@gmail.com>
2023-06-22 23:10:12 +0600

    Made getUserInfo function in user.service

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -8,8 +8,8 @@ export class UserService {
 
   constructor(private http: HttpClient) { }
 
-  // getUserInfo(slug_name: string){
-  //   const url = `http://localhost:8000/api/user/by_slug/${slug_name}/`
-  //   this.http.get<User>(url);
-  // }
+  getUserInfo(slug_name: string){
+    const url = `http://localhost:8000/api/user/by_slug/${slug_name}/`
+    this.http.get<User>(url);
+  }
 }
